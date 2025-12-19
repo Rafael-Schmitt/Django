d3962ebebc63936a359a1969a8169e7866f8b58a
Rafael <rafaelschmi@gmail.com>
2025-12-19 17:56:05 +0800

    changes

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = [
 ]
 
 # For production - where static files will be collected when you run collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
